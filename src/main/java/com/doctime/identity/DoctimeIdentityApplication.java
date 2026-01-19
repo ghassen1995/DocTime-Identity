f247a2de97a50967836adafcd6b79e2a19041588
Ghassen Kahri <ghassen.kahri@gtt-tunisie.tn>
2026-01-20 00:42:49 +0100

    initial commit

---
+++ src/main/java/com/doctime/identity/DoctimeIdentityApplication.java
@@ -0,0 +1,14 @@
+package com.doctime.identity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class DoctimeIdentityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctimeIdentityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/doctime/identity/DoctimeIdentityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doctime.identity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctimeIdentityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
