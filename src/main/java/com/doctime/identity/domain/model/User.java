fb443c484019d46847c94aae27209fc75e95dcd0
Ghassen Kahri <ghassen.kahri@gtt-tunisie.tn>
2026-01-20 01:12:43 +0100

    removed the jpa annotation from domain user class

---
+++ src/main/java/com/doctime/identity/domain/model/User.java
@@ -0,0 +1,69 @@
+package com.doctime.identity.domain.model;
+
+public class User {
+    private Long id;
+    private String email;
+    private String passwordHash;
+    private String firstName;
+    private String lastName;
+    private String role;
+
+    public User() {}
+
+    public User(Long id, String email, String passwordHash, String firstName, String lastName, String role) {
+        this.id = id;
+        this.email = email;
+        this.passwordHash = passwordHash;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.role = role;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
